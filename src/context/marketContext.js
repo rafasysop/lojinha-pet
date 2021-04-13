df30ec621feaa79d190ee6ab620b9c6561ee2afd
rafasysop <rafaelcostamoura@gmail.com>
2021-04-13 07:54:50 -0300

    Starting project and defining structures

---
+++ src/context/marketContext.js
@@ -0,0 +1,14 @@
+const InitialState = {
+  market: [
+    {
+      product_id: 5,
+      product_name: 'algum',
+      product_description: 'alguma descrição',
+      product_category: 'brinquedos',
+      product_discount: 0,
+      product_price: 69.99,
+      product_amount: 8,
+      user_observation: 'alguma observação',
+    },
+  ],
+};
